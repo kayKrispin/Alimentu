1e69e812a1e0a5f9c58701b8d7ff422abd93c545
taras bashuryn <tarasbashuryn@gmail.com>
2018-12-15 14:37:57 +0200

    config-file

--- src/server/src/config.js
+++ src/server/src/config.js
@@ -2,11 +2,11 @@ require('dotenv').config({path:'C:/Users/User/Desktop/alimentu/src/server/.env'}
 
 
 module.exports = {
-    facebookAppId: process.env.REACT_APP_FACEBOOK_APP_ID,
-    googleAppId:process.env.REACT_APP_GOOGLE_APP_ID,
-    facebookAppSecret: process.env.REACT_APP_FACEBOOK_APP_SECRET,
-    oauthRedirectUrl:process.env.REACT_APP_OAUTH_REDICRECT_URL,
-    jwt_secret: 'fuck_you_kaec',
-    sendgridApiKey:process.env.REACT_APP_SENDGRID_API_KEY,
-    fromEmail:process.env.REACT_APP_FROM_EMAIL,
+    facebookAppId: process.env.REACT_APP_FACEBOOK_APP_ID ,
+    googleAppId:process.env.REACT_APP_GOOGLE_APP_ID ,
+    facebookAppSecret: process.env.REACT_APP_FACEBOOK_APP_SECRET ,
+    oauthRedirectUrl:process.env.REACT_APP_OAUTH_REDICRECT_URL ,
+    jwt_secret: 'fuck_you_kaec' ,
+    sendgridApiKey:process.env.REACT_APP_SENDGRID_API_KEY ,
+    fromEmail:process.env.REACT_APP_FROM_EMAIL ,
 };
